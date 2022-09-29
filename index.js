4522d571fa91520da4cc46720f90ace15e69c15d
Dydou28 <dylannail@orange.fr>
2022-09-29 12:08:38 +0200

    node

---
+++ index.js
@@ -0,0 +1,3 @@
+import chalk from "chalk";
+
+console.log(chalk.red.bgBlue.bold("Valentin") + chalk.red.bgGrey.bold("Maeva") + chalk.yellow.bgRed.bold("Dylan") + chalk.green.bgYellow.bold("Joryan"));
